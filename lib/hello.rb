63a1e149feb97699ff9b08a0371704f9e893503a
Diane Nguyen <diane.nguyen@gmail.com>
2021-08-02 21:38:16 -0700

    Yield and Blocks assignment completed.

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,18 +13,3 @@ def hello_t(names)
         puts "Hey! No block was given!"
     end
 end
-
-# def hello_t(names)
-#     if block_given?
-#         names.each do |name| puts "#{name}"
-#         end
-#         return names 
-#     else 
-#         # yield
-#         # block_given =   
-#         puts "Hey! No block was given!"
-#         # block_given
-    
-#     end
-# # end
-# end
