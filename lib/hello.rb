1429c151c2a42ad4f5fe2327ec69fbf44353df7c
Diane Nguyen <diane.nguyen@gmail.com>
2021-08-02 21:31:36 -0700

    Yield and Blocks assignment completed.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,30 @@
-def hello_t
 
+def hello_t(names)
+    if block_given?
+        i = 0
+      
+        while i < names.length
+            yield(names[i])
+            i += 1
+        end
+        names
+        
+    else
+        puts "Hey! No block was given!"
+    end
 end
 
-# call your method here!
-
+# def hello_t(names)
+#     if block_given?
+#         names.each do |name| puts "#{name}"
+#         end
+#         return names 
+#     else 
+#         # yield
+#         # block_given =   
+#         puts "Hey! No block was given!"
+#         # block_given
+    
+#     end
+# # end
+# end
